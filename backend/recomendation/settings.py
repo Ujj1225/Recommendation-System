1f0475b33fe1e0118c944722d9b7ae84c34f15df
Sandesh Pyakurel <077bct075.sandesh@pcampus.edu.np>
2023-03-16 10:50:51 +0545

    cors fixed

--- backend/recomendation/settings.py
+++ backend/recomendation/settings.py
@@ -143,9 +143,12 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES':(
         'rest_framework.renderers.JSONRenderer'
     ),
+    'DEFAULT_PERMISSION_CLASSES': (  
+        'rest_framework.permissions.AllowAny' 
+        ),
 }
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=200),
